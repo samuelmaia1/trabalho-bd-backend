e3c8113dc916101a1047c779bec9acd156f64319
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-11-24 01:36:53 -0300

    Começando endpoint de produtos

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import userController from './routes/User.js'
+import productController from './routes/Product.js'
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 app.use('/usuarios', userController)
+app.use('/produtos', productController)
 
 app.get('/', async (req, res) => {
     
