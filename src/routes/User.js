7597e10a3cde1026e1fbba3fae91c0f0cae8303c
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-11-24 00:46:41 -0300

    Controller de usuário pronto

--- src/routes/User.js
+++ src/routes/User.js
@@ -4,14 +4,51 @@ import { UserService } from '../services/UserService.js'
 const router = express.Router()
 const service = new UserService()
 
+router.get('/', async (req, res) => {
+    try {
+        return res.status(200).json(await service.getAllUsers())
+    } catch (error) {
+        return res.status(500).json({erro: 'Erro interno ao processar usuários. Por favor, tente novamente mais tarde.'})
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        return res.status(200).json(await service.getUserById(req.params.id))
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({erro: 'Erro interno ao processar usuário. Por favor, tente novamente mais tarde.'})
+    }
+})
+
 router.post('/criar', async (req,res) => {
     try {
         return res.status(201).json(await service.createUser(req.body)) 
     } catch (error) {
-        if (error.message == 'Todos os campos devem ser preenchidos.')
+        console.log(error.message)
+        if (error.message.includes('viola a restrição de não-nulo'))
             return res.status(400).json({erro: 'Preencha todos os campos'})
+        return res.status(500).json({erro: 'Erro interno. Por favor, tente novamente mais tarde.'})
     }
     
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedUser = await service.deleteUser(req.params.id)
+        return res.status(204).json({ok: true, message: 'Usuário deletado com sucesso', data: deletedUser})
+    } catch (error) {
+        return res.status(500).json({erro: 'Erro ao excluir conta. Por favor, tente novamente mais tarde.'})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        return res.status(200).json(await service.updateUser(req.body, req.params.id))
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({erro: 'Erro interno. Por favor, tente novamente mais tarde.'})
+    }
 })
 
 export default router
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -5,12 +5,52 @@ import bcrypt from 'bcryptjs'
 export class UserService{
     async createUser(userData){
         const {name, email, password, street, neighborhood, city} = userData
+        console.log(name + ' ' + password + ' ' + street + ' ' +  neighborhood + ' ' + city)
 
-        if (!name || !email || !password || !street || !neighborhood || city) throw new Error('Todos os campos devem ser preenchidos.')
+        if (!name || !email || !password || !street || !neighborhood || !city) throw new Error('Todos os campos devem ser preenchidos.')
 
         const queryString = 'INSERT INTO usuario (id, nome, email, senha, rua, bairro, cidade) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;'
 
         const res = await query(queryString, [randomUUID(), name, email, await bcrypt.hash(password, 10), street, neighborhood, city])
+
+        return res.rows[0]
+    }
+
+    async getUserById(id){
+        const queryString = 'SELECT id, nome, email, rua, bairro, cidade FROM usuario WHERE id = $1;'
+
+        const res = await query(queryString, [id])
+
+        return res.rows[0]
+    }
+
+    async getAllUsers(){
+        const queryString = 'SELECT * FROM usuario;'
+
+        const res = await query(queryString)
+
+        return res.rows
+    }
+
+    async deleteUser(id){
+        const queryString = 'DELETE FROM usuario WHERE id = $1 RETURNING *'
+
+        const res = await query(queryString, [id])
+
+        return res.rows[0]
+    }
+
+    async updateUser(data, id){
+        const keys = Object.keys(data)
+        const values = Object.values(data)
+
+        if (keys.length == 0)
+            throw new Error('Nenhum campo para atualizar.')
+
+        const setQuery = keys.map((key, i) => `${key} = $${i+1}`).join(', ')
+        const queryString = `UPDATE usuario SET ${setQuery} WHERE id = $${keys.length + 1} RETURNING id, nome, email`
+
+        const res = await query(queryString, [...values, id])
 
         return res.rows[0]
     }
