d7eab153c54936806f72ca728b34fff0c6d0db26
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-11-23 23:39:06 -0300

    Rota de criação de usuário

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 import express from 'express'
-import db from './db/db-config.js'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import userController from './routes/User.js'
+
+dotenv.config()
 
 const app = express()
+
+app.use(cors())
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.use('/usuarios', userController)
 
 app.get('/', async (req, res) => {
     
 })
 
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Rodando na porta ${process.env.SERVER_PORT}`)
 })
